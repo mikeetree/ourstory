9d41b1d1027e437cc020192482b7f5d4e431a566
Your Name <mikeetree@gmail.com>
2021-02-16 15:41:29 -0500

    Initial client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import PreviewsView from './PreviewsView';
+import StoryView from './StoryView';
+import CreateView from './CreateView';
+
+export default function App() {
+  const [currentStory, setCurrentStory] = useState('');
+  const [currentView, setCurrentView] = useState('previews');
+
+  function setStoryAndView(id) {
+    setCurrentStory(id);
+    setCurrentView('story');
+  }
+
+  let view;
+
+  if (currentView === 'previews') {
+    view = (
+      <>
+        <button onClick={() => setCurrentView('create')}>Begin a story</button>
+
+        <PreviewsView textLength={10} clickHandler={setStoryAndView} />
+      </>
+    );
+  } else if (currentView === 'story') {
+    view = <StoryView id={currentStory} />;
+  } else if (currentView === 'create') {
+    view = <CreateView submitHandler={setStoryAndView} />;
+  }
+
+  return (
+    <div>
+
+      {view}
+
+    </div>
+  );
+}
